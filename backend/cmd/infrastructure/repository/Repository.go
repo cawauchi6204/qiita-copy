cad62a0fb74f0cc8e5f9008454cefbbb0ad64cf3
cawauchi6204 <cawauchi6204@gmail.com>
2022-08-31 21:59:58 +0900

    I: Repository作成途中まで

---
+++ backend/cmd/infrastructure/repository/Repository.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func dbConnect() *gorm.DB {
+	dsn := "user:password@tcp(db:3306)/test_db"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+var DB = dbConnect()
