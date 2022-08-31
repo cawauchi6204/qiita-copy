32f3f63183ddcbdf731a562cc92c85e88c8334b1
cawauchi6204 <cawauchi6204@gmail.com>
2022-08-31 17:48:40 +0900

    I: nginxで配信するようにした

--- backend/main.go
+++ backend/main.go
@@ -1,68 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"os"
 
-	_ "github.com/go-sql-driver/mysql"
-	"xorm.io/xorm"
+	"github.com/gin-gonic/gin"
 )
 
-type User struct {
-	ID   int64  `xorm:"id pk autoincr"`
-	Name string `xorm:"name"`
-	Age  int    `xorm:"age"`
-}
-
-// createTable テーブルを作成する
-func createTable(engine xorm.Engine) {
-	err := engine.CreateTables(User{})
-	if err != nil {
-		log.Fatalf("テーブルの生成に失敗しました。: %v", err)
-	}
-	fmt.Println("テーブル作成が成功しました。")
-}
-
-// insert テーブルにレコードを追加する
-func insert(engine xorm.Engine) {
-	user := User{
-		Name: "tanaka",
-		Age:  20,
-	}
-	_, err := engine.Table("user").Insert(user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("レコードの追加が完了しました。")
-}
-
-// get 単体取得(1レコードを取得)
-func get(engine xorm.Engine) {
-	user := User{}
-	// idフィールドは Autoincrement なので Insert済みなのであれば id = 1 のユーザが取得できるはず
-	result, err := engine.ID(1).Get(&user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if !result {
-		log.Fatal("ユーザーが見つかりませんでした。")
-	}
-	fmt.Printf("取得したレコード :%+v\n", user)
-}
-
 func main() {
-	//engineを作成します。
-	engine, err := xorm.NewEngine("mysql", "root:password@tcp(db:3306)/test_db?charset=utf8mb4&parseTime=true")
-	if err != nil {
-		log.Fatal(err)
+	r := gin.Default()
+	deployPort := os.Getenv("PORT")
+	if deployPort == "" {
+		deployPort = "8080"
 	}
-
-	//テーブルを作成する
-	createTable(*engine)
-	//テーブルに追加する
-	insert(*engine)
-	//テーブルから取得する
-	get(*engine)
-
-	fmt.Println("うまく動きました。")
+	r.Run(":" + deployPort)
 }
