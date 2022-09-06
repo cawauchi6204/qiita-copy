c0483e176eac1a425eaed27e5d377c5e34bd0782
cawauchi6204 <cawauchi6204@gmail.com>
2022-09-06 14:44:03 +0900

    A: 記事カードコンポーネント

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import Layout from "../components/Layout"
+import CardList from "../components/CardList"
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <main>
-        <button className="bg-green-600 hover:bg-green-500 text-white rounded px-4 py-2">登録する</button>
+        <CardList />
       </main>
     </Layout>
   )
