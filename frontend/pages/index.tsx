b11dff1b54f2462695e943f873ca5d4769fc0cfa
cawauchi <59794909+cawauchi6204@users.noreply.github.com>
2022-09-06 14:04:50 +0900

    A: Footer (#31)

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 const Home: NextPage = () => {
   return (
@@ -17,19 +18,7 @@ const Home: NextPage = () => {
         <Header />
         <button className="bg-green-600 hover:bg-green-500 text-white rounded px-4 py-2">登録する</button>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
