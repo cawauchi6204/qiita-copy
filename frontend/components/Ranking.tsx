b4a99bbd613a9e609361ffd53af444237e741910
cawauchi6204 <cawauchi6204@gmail.com>
2022-09-06 15:17:27 +0900

    A: ランキングコンポーネント

---
+++ frontend/components/Ranking.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link"
+
+type Props = {
+  rankingTitle: string
+  rankingUnit: string
+  contents: RankingContent[]
+}
+
+type RankingContent = {
+  img: string
+  name: string
+  count: number
+}
+
+const Ranking: React.FC<Props> = ({ rankingTitle, rankingUnit, contents }) => {
+  return (
+    <div className="w-full">
+      <span className="font-bold">{rankingTitle}</span>
+      <span>週間</span>
+      <span>月間</span>
+      <ul className="flex flex-col mt-4 p-2 justify-between">
+        {contents.map((content, index) => {
+          return (
+            <Link href="">
+              <a>
+                <li className="flex gap-8 hover:bg-gray-200">
+                  <span>
+                    {index + 1}
+                  </span>
+                  <span>
+                    {content.name}
+                  </span>
+                  <span>
+                    <span className="font-bold">{content.count}</span>
+                    <span>{rankingUnit}</span>
+                  </span>
+                </li>
+              </a>
+            </Link>
+          )
+        })}
+      </ul>
+      <hr />
+    </div>
+  )
+}
+
+export default Ranking
