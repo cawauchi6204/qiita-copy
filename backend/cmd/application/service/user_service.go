eafd3b90a938144ae956c7a69d3cfc2104a147e1
cawauchi6204 <cawauchi6204@gmail.com>
2022-09-09 11:41:49 +0900

    I: 修正

--- backend/cmd/application/service/user_service.go
+++ backend/cmd/application/service/user_service.go
@@ -22,10 +22,10 @@ func GetUserByEmail(email string) (user repository.User) {
 	return
 }
 
-func CreateUser(id, email, password string) (result *gorm.DB) {
+func CreateUser(name, email, password string) (result *gorm.DB) {
 	// TODO: これはEntityに持たせたい
 	u := repository.User{
-		ID:             id,
+		ID:             name,
 		Email:          email,
 		Password:       password,
 		Description:    "",
@@ -37,39 +37,5 @@ func CreateUser(id, email, password string) (result *gorm.DB) {
 		CreatedAt:      time.Now(),
 	}
 	repository.CreateUser(u)
-}
-
-func Signup(id, email, password string) (*repository.User, error) {
-	user := repository.User{}
-	repository.FindUserByEmail(email)
-	if user.ID != "" {
-		err := errors.New("同一名のメールアドレスが既に登録されています。")
-		fmt.Println(err)
-		return nil, err
-	}
-
-	encryptPw, err := crypto.PasswordEncrypt(password)
-	if err != nil {
-		fmt.Println("パスワード暗号化中にエラーが発生しました。：", err)
-		return nil, err
-	}
-	createUser(id, email, encryptPw)
-	return &user, nil
-}
-
-func Login(email, password string) (*repository.User, error) {
-	user := repository.FindUserByEmail(email)
-	if user.ID == "" {
-		err := errors.New("メールアドレスが一致するユーザーが存在しません")
-		fmt.Println(err)
-		return nil, err
-	}
-
-	err := crypto.CompareHashAndPassword(user.Password, password)
-	if err != nil {
-		fmt.Println("パスワードが一致しませんでした。:", err)
-		return nil, err
-	}
-
-	return &user, nil
+	return
 }
