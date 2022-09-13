532b4f0b9f952f230c5949a36370f5048479b8da
cawauchi6204 <cawauchi6204@gmail.com>
2022-09-13 17:11:39 +0900

    I: テーブル情報修正

---
+++ backend/cmd/infrastructure/repository/tag_repository.go
@@ -0,0 +1,6 @@
+package repository
+
+type Tag struct {
+	ID     string `json:"id"`
+	ImgUrl string `json:"imgUrl"`
+}
--- db/migrations/migrations/000001_create_users_table.up.sql
+++ db/migrations/migrations/000001_create_users_table.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE
     name VARCHAR (50) NOT NULL,
     password VARCHAR (300) NOT NULL,
     email VARCHAR (300) UNIQUE NOT NULL,
+    img_url TEXT NOT NULL,
     description TEXT NOT NULL,
     hp_url VARCHAR(100) NOT NULL,
     location VARCHAR(50) NOT NULL,
